681d6064ddb55847f6e383f7fe58aa4b06b1aaed
alumno <alumno>
2018-08-21 12:15:37 -0300

    Primera clase

---
+++ Manassali.Leonardo.2A/Ejercicio01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio01
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            string nombre;
+            int edad;
+
+            Console.WriteLine("Ingrese nombre:");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese edad:");
+            edad = int.Parse(Console.ReadLine());
+            Console.WriteLine("Su nombre es {0} y su edad {1}.",nombre,edad);
+        }
+    }
+}
