b23d6361f5c459f1a71bf99ba1ced4551e49a242
Xargam <leoxargam@gmail.com>
2018-08-28 15:32:12 -0300

    Ejercicio 13 casi terminado

--- Manassali.Leonardo.2A/Ejercicio 13/Conversor.cs
+++ Manassali.Leonardo.2A/Ejercicio 13/Conversor.cs
@@ -12,56 +12,62 @@ double BinarioDecimal(string). Convierte un número binario a decimal.
 
 namespace Datos
 {
-  public static class Conversor
-  {
-    public static string DecimalBinario(double num)
-    {
-      string binario = "";
-      string aux = "";
-      long entero = (long)num;
-      num -= entero;
+	public static class Conversor
+	{
+		public static string DecimalBinario(double num)
+		{
+			string binario = "";
+			string aux = "";
+			long entero = (long)num;
+			num -= entero;
 
-      while (entero > 1)
-      {
-        aux += entero % 2;
-        binario = binario.Insert(0, aux);
-        aux = "";
-        entero /= 2;
-      }
-      binario = binario.Insert(0, "1");
-      if (num > 0)
-      {
-        binario += ",";
-        while (num > 0)
-        {
-          num *= 2;
-          entero = (long)num;
-          binario += entero;
-          num -= entero;
-        }
-      }
-      return binario;
-    }
+			while (entero > 1)
+			{
+				aux += entero % 2;
+				binario = binario.Insert(0, aux);
+				aux = "";
+				entero /= 2;
+			}
+			binario = binario.Insert(0, "1");
+			if (num > 0)
+			{
+				binario += ",";
+				while (num > 0)
+				{
+					num *= 2;
+					entero = (long)num;
+					binario += entero;
+					num -= entero;
+				}
+			}
+			return binario;
+		}
 
-    public static double BinarioDecimal(string num)
-    {
-      double numDecimal = 0;
-      long valorDecimal;
-      string aux;
+		public static double BinarioDecimal(string num)
+		{
+			double numDecimal = 0;
+			string aux;
+			int point = num.IndexOf('.');
+			bool isDecimal = false; 
 
-      for (int i = 0; i < num.Length; i++)
-      {
-        aux = num.Substring(i , 1);
-        if (aux == "1")
-        {
-          valorDecimal = (long)Math.Pow(2, num.Length-1-i );
-          numDecimal += valorDecimal;
-        }
-        aux = "";
-      }
+			point = (point < 0) ? num.IndexOf(',') : point;
+			point = (point < 0) ? num.Length : --point;
 
-      return numDecimal;
-
-    }
-  }
+			for (int i = 0; i < point+1; i++)
+			{
+				aux = num.Substring(i, 1);
+				if (aux == "1")
+				{
+					numDecimal += Math.Pow(2, (point - i) );
+				}
+				aux = "";
+				if (i == point && num.Length > point)
+				{
+					num = num.Remove(point+1, 1);
+					point += (num.Length - point);
+				}
+			}
+			return numDecimal;
+		}
+	}
 }
