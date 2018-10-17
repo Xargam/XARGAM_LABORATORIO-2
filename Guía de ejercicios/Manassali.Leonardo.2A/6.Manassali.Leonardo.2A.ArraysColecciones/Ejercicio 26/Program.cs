9975482f54684b2695b14edaa49afc31326993b6
Xargam <leoxargam@gmail.com>
2018-10-16 22:09:45 -0300

    Correccion del ejercicio 26

--- Guía de ejercicios/Manassali.Leonardo.2A/6.Manassali.Leonardo.2A.ArraysColecciones/Ejercicio 26/Program.cs
+++ Guía de ejercicios/Manassali.Leonardo.2A/6.Manassali.Leonardo.2A.ArraysColecciones/Ejercicio 26/Program.cs
@@ -5,10 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 
 /*
-26. Crear una aplicación de consola que cargue 20 números enteros (positivos y negativos) distintos de cero de forma aleatoria utilizando la clase Random.
+26. Crear una aplicación de consola que cargue 20 números enteros (vector y vector) distintos de cero de forma aleatoria utilizando la clase Random.
 a. Mostrar el vector tal como fue ingresado
-b. Luego mostrar los positivos ordenados en forma decreciente.
-c. Por último, mostrar los negativos ordenados en forma creciente.
+b. Luego mostrar los vector ordenados en forma decreciente.
+c. Por último, mostrar los vector ordenados en forma creciente.
 */
 
 namespace Ejercicio_26
@@ -18,55 +18,57 @@ namespace Ejercicio_26
 		static void Main(string[] args)
 		{
 			int[] vector = new int[20];
-			int[] positivos = new int[20];
-			int[] negativos = new int[20];
 			int aux;
 			Random numAleatorio = new Random();
 
 			Console.Title = "Ejercicio 26";
-			Console.WriteLine("El vector:");
-			for (int i = 0,j = 0,k = 0; i < 20; i++)
+			Console.WriteLine("El vector inicial:");
+			for (int i = 0; i < vector.Length; i++)
 			{
 				vector[i] = numAleatorio.Next(-1000,1000);
-				if( vector[i] > 0)
-				{
-					positivos[j++] = vector[i];
-				}
-				else
-				{
-					negativos[k++] = vector[i];
-				}
-				Console.WriteLine(vector[i]);
 			}
-			Console.WriteLine("Ordenamiento decreciente:");
-			for (int i = 0; i < positivos.Length-1 ; i++)
+			Program.MostrarVector(vector);
+
+			Console.WriteLine("Ordenamiento decreciente de los positivos:");
+			for (int i = 0; i < vector.Length-1 ; i++)
 			{
-				for(int j = i+1; j < positivos.Length; j++ )
+				for(int j = i+1; j < vector.Length; j++ )
 				{
-					if( positivos[i] < positivos[j] )
+					if( vector[i] < vector[j] && vector[j] > 0)
 					{
-						aux = positivos[i];
-						positivos[i] = positivos[j];
-						positivos[j] = aux;
+						aux = vector[i];
+						vector[i] = vector[j];
+						vector[j] = aux;
 					}
 				}
-				Console.WriteLine(positivos[i]);
 			}
-			Console.WriteLine("Ordenamiento creciente:");
-			for (int i = 0; i < negativos.Length-1; i++)
+			Program.MostrarVector(vector);
+
+			Console.WriteLine("Ordenamiento creciente de los negativos:");
+			for (int i = 0; i < vector.Length-1; i++)
 			{
-				for (int j = i + 1; j < negativos.Length; j++)
+				for (int j = i + 1; j < vector.Length; j++)
 				{
-					if (negativos[i] > negativos[j])
+					if (vector[i] > vector[j] && vector[j] < 0)
 					{
-						aux = negativos[i];
-						negativos[i] = negativos[j];
-						negativos[j] = aux;
+						aux = vector[i];
+						vector[i] = vector[j];
+						vector[j] = aux;
 					}
 				}
-				Console.WriteLine(negativos[i]);
+				
 			}
+			Program.MostrarVector(vector);
 			Console.ReadLine();
+
+		}
+
+		private static void MostrarVector(int[] vector)
+		{
+			foreach (int item in vector)
+			{
+				Console.WriteLine(item);
+			}
 		}
 	}
 }
