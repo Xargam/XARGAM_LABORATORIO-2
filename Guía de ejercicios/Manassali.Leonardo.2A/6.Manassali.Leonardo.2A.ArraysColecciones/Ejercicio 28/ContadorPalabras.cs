0d689bd9896d6a58c77b793f3982710daf1e74a7
Xargam <leoxargam@gmail.com>
2018-10-29 04:05:43 -0300

    Revision y correccion: Arrays y colecciones

--- Guía de ejercicios/Manassali.Leonardo.2A/6.Manassali.Leonardo.2A.ArraysColecciones/Ejercicio 28/ContadorPalabras.cs
+++ Guía de ejercicios/Manassali.Leonardo.2A/6.Manassali.Leonardo.2A.ArraysColecciones/Ejercicio 28/ContadorPalabras.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
-using System.Windows.Forms;
-using System;
 using System.Text;
+using System.Windows.Forms;
 
 /*
 28. Generar un WindowsForm con un RichTextBox y un botón, con el siguiente formato:
@@ -31,17 +30,16 @@ namespace Ejercicio_28
 			Dictionary<string, int> diccionario = new Dictionary<string, int>();
 			string texto = this.rtbTexto.Text;
 			StringBuilder datos = new StringBuilder();
-
-			//Elimino espacios de mas.
-			texto = texto.Trim();
-			while ( texto.Contains("  ") )
+			if ( texto != "" )
 			{
-				texto = texto.Replace("  ", " ");
-			}
-			//Recorro las palabras y determino el valor de su contador
-			foreach ( string palabra in texto.Split() )
-			{
-				if ( palabra != "" )
+				//Elimino espacios de mas.
+				texto = texto.Trim();
+				while ( texto.Contains("  ") )
+				{
+					texto = texto.Replace("  ", " ");
+				}
+				//Recorro las palabras y determino el valor de su contador
+				foreach ( string palabra in texto.Split() )
 				{
 					if ( diccionario.ContainsKey(palabra) )
 					{
@@ -53,38 +51,43 @@ namespace Ejercicio_28
 						//Guardo la palabra en lista para poder recorrer el diccionario.
 						claves.Add(palabra);
 					}
-				}
-			}
-			while ( diccionario.Count > 0 )
-			{
-				//Recorro el diccionario determinando el valor mas grande con su clave asociada
-				foreach ( string item in claves )
-				{
-					if ( diccionario[item] > maxValue )
-					{
-						maxValue = diccionario[item];
-						maxValueKey = item;
-					}
-				}
-				//Elimino de lista para que dicha clave no se reconsidere como una posible
-				claves.Remove(maxValueKey);
-				//Agrego a las claves ordenadas la palabra asociada al valor mas grande
-				clavesOrdenadas.Add(maxValueKey);
-				//Elimino la clave y el valor del diccionario para que el contador de elementos baje
-				diccionario.Remove(maxValueKey);
-				//Agrego el valor y la clave a un diccionario ordenado de manera descendente
-				diccionarioOrdenado.Add(maxValueKey, maxValue);
-				maxValue = 0;
-			}
 
-			//Recorro el diccionario ordenado y almaceno su contenido para mostrarlo
-			datos.AppendLine("Palabra | Cantidad");
-			for ( int i = 0 ; i < 3 ; i++ )
-			{
-				datos.AppendLine(string.Format("{0}\t   {1}", clavesOrdenadas[i], diccionarioOrdenado[clavesOrdenadas[i]]));
+				}
+				while ( diccionario.Count > 0 )
+				{
+					//Recorro el diccionario determinando el valor mas grande con su clave asociada
+					foreach ( string item in claves )
+					{
+						if ( diccionario[item] > maxValue )
+						{
+							maxValue = diccionario[item];
+							maxValueKey = item;
+						}
+					}
+					//Elimino de lista para que dicha clave no se reconsidere como una posible
+					claves.Remove(maxValueKey);
+					//Agrego a las claves ordenadas la palabra asociada al valor mas grande
+					clavesOrdenadas.Add(maxValueKey);
+					//Elimino la clave y el valor del diccionario para que el contador de elementos baje
+					diccionario.Remove(maxValueKey);
+					//Agrego el valor y la clave a un diccionario ordenado de manera descendente
+					diccionarioOrdenado.Add(maxValueKey, maxValue);
+					maxValue = 0;
+				}
+
+				//Recorro el diccionario ordenado y almaceno su contenido para mostrarlo
+				datos.AppendLine("Palabra | Cantidad");
+				for ( int i = 0 ; i < 3 ; i++ )
+				{
+					if ( i == clavesOrdenadas.Count )
+					{
+						break;
+					}
+					datos.AppendLine(string.Format("{0}\t   {1}", clavesOrdenadas[i], diccionarioOrdenado[clavesOrdenadas[i]]));
+				}
+				MessageBox.Show(datos.ToString());
 			}
-			MessageBox.Show(datos.ToString());
-		} 
+		}
 		#endregion
 	}
 }
