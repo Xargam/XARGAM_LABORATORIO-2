392ff06f7abf6fdbcac868d145ef2b6791b36ab9
Xargam <leoxargam@gmail.com>
2018-11-12 01:59:58 -0300

    Ejercicio 68 completado

---
+++ Guía de ejercicios/Manassali.Leonardo.2A/Manassali.Leonardo.2A.Eventos/Ejercicio 68/FormPersona.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Ejercicio_68
+{
+    public partial class FormPersona : Form
+    {
+        #region Atributos
+        private Persona _persona;
+        private event DelegadoString _personaCreada;
+        #endregion
+
+        #region Constructores
+        public FormPersona()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Metodos
+        private void Persona_Load(object sender, EventArgs e)
+        {
+            this._personaCreada += new DelegadoString(FormPersona.NotificarCambio);
+        }
+
+
+        private static void NotificarCambio(string mensaje)
+        {
+            MessageBox.Show(mensaje);
+        }
+
+        private void btnCrear_Click(object sender, EventArgs e)
+        {
+            string mensaje = "La persona se actualizó";
+            if (object.Equals(this._persona , null))
+            {
+                mensaje = "Se ha creado la persona.";
+                this.btnCrear.Text = "Actualizar";
+            }
+            else if(this._persona.Apellido == this.txtApellido.Text && this._persona.Nombre == this.txtNombre.Text)
+            {
+                mensaje = "La persona fue recargada sin alteraciones.";
+            }
+            Persona persona = new Persona(this.txtNombre.Text, this.txtApellido.Text);
+            this._personaCreada(mensaje + "\r\n" + persona.Mostrar());
+            this._persona = persona;
+
+        }
+        #endregion
+    }
+}
---
+++ Guía de ejercicios/Manassali.Leonardo.2A/Manassali.Leonardo.2A.Eventos/Ejercicio 68/Persona.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Ejercicio_68
+{
+    public delegate void DelegadoString(string msg);
+    
+    public class Persona
+    {
+        #region Atributos
+        private string _apellido;
+        private string _nombre;
+        #endregion
+
+        #region Propiedades
+        public string Apellido
+        {
+            get
+            {
+                return this._apellido;
+            }
+            set
+            {
+                this._apellido = value;
+            }
+        }
+        public string Nombre
+        {
+            get
+            {
+                return this._nombre;
+            }
+            set
+            {
+                this._nombre = value;
+            }
+        }
+        #endregion
+
+        #region Constructores
+        public Persona(string nombre , string apellido)
+        {
+            this.Nombre = nombre;
+            this.Apellido = apellido;
+        }
+        #endregion
+
+        #region Metodos
+        public string Mostrar()
+        {
+            return this.Nombre + " - " + this.Apellido + "\n";
+        }
+        #endregion
+    }
+}
